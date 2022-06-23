88ae70ddeeb8613976cb4023aa39b5b6b20cefa5
LykovaKateryna <k.lykova.ip15@kpi.ua>
2022-06-23 03:47:37 +0300

    i

---
+++ lab3/cpp/Num_arr.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "func.h"
+class Num_arr
+{
+public:
+	Num_arr();
+	Num_arr(int);
+	void print_arr();
+	void set_num_el(int);
+	int get_num_el();
+	void set_arr();
+	double* get_arr();
+	double find_max_el();
+	void delete_arr();
+private:
+	int num_el;
+	double* arr;
+};
