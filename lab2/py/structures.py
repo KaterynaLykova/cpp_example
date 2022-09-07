59f6852054603e99f400c8d77634ced0e44d7622
LykovaKateryna <katya.lykova032@gmail.com>
2022-09-07 22:50:25 +0300

    del

--- lab2/py/structures.py
+++
@@ -1,9 +0,0 @@
-class Time:
-    def __init__(time, hours, minutes):
-        time.hours = hours
-        time.minutes = minutes
-class TvShow:
-    def __init__(show, title, start, end):
-        show.title = title
-        show.start = start
-        show.end  = end
